60e76b3410c39035afbfcd43db12db34d206012c
csx-briarcliff <csx.briarcliff@gmail.com>
2025-02-09 17:06:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById("navbarLogo").onclick = function () {
+  window.location.href = "index.html";
+};
+
+document.getElementById("linkTO-sprout").onclick = function () {
+  window.location.href = "sprout.html";
+};
+document.getElementById("linkTO-join").onclick = function () {
+  window.location.href = "null.html";
+};
+document.getElementById("linkTO-future").onclick = function () {
+  window.location.href = "null.html";
+};
+document.getElementById("linkTO-contact").onclick = function () {
+  window.location.href = "contact.html";
+};
+
+document.getElementById("linkTO-youtube").onclick = function () {
+  window.location.href = "https://www.youtube.com/@CSXBriarcliff";
+};
+document.getElementById("linkTO-facebook").onclick = function () {
+  window.location.href =
+    "https://www.facebook.com/profile.php?id=61572859319399";
+};
